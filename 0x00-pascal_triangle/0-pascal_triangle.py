4b2f72647007b346e6d7d974cfe26ab00d1174d5
haddyadnan <haddyadnan@gmail.com>
2022-12-20 17:07:58 +0100

    black refactor

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """
 pascal traingle
+TODO: refactor loop
 """
+
+
 def pascal_triangle(n):
+
+    """pascal triangle V0.1"""
 
     result = []
     if n <= 0:
@@ -12,12 +17,12 @@ def pascal_triangle(n):
         return result
     else:
         result.append([1])
-        for i in range(n - 1): #TODO: refactor loop for faster time
+        for i in range(n - 1):
             prev = result[-1]
             tmp = []
             index = 0
-            for i in range(len(prev) -1):
-                tmp.append(prev[index] + prev[index+1])
+            for i in range(len(prev) - 1):
+                tmp.append(prev[index] + prev[index + 1])
                 index += 1
             tmp.insert(0, 1)
             tmp.append(1)
